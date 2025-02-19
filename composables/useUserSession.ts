c79bb4ef0f39b2807cf097b31313348886dedd7e
Guilherme Cazaroto <guilherme.cazaroto@sek.io>
2025-02-19 11:21:33 -0300

    refactor

--- composables/useUserSession.ts
+++ composables/useUserSession.ts
@@ -3,21 +3,7 @@ import { appendResponseHeader } from 'h3'
 export const useUserSession = () => {
   const session = useState<any>('session', () => null);
   const serverEvent = import.meta.server ? useRequestEvent() : null
-
-  const clearSession = async () => {
-    const res = await $fetch.raw('/api/auth', {
-      method: 'DELETE',
-    });
-
-    const cookies = res.headers.getSetCookie()
-    if (import.meta.server && serverEvent) {
-      for (const cookie of cookies) {
-        appendResponseHeader(serverEvent, 'set-cookie', cookie)
-      }
-    }
-
-    session.value = null;
-  };
+  const loggedIn = computed(() => !!session.value);
 
   const fetch = async () => {
     session.value = await useRequestFetch()('/api/auth', {
@@ -28,8 +14,22 @@ export const useUserSession = () => {
     })
   }
 
+  const clearSession = async () => {
+    const res = await $fetch.raw('/api/auth', {
+      method: 'DELETE',
+    });
 
-  const loggedIn = computed(() => !!session.value);
+    const cookies = res.headers.getSetCookie()
+    
+    if (import.meta.server && serverEvent) {
+      for (const cookie of cookies) {
+        appendResponseHeader(serverEvent, 'set-cookie', cookie)
+      }
+    }
+
+    session.value = null;
+  };
+
   
   return { session, clearSession, loggedIn, fetch };
 };
